0d2951912dfc6cf432f858bc082030ada7676cee
David Vannerom <david.vannerom@cern.ch>
2017-08-10 15:02:20 +0200

    Added comments in RunSelector.C

--- RunSelector.C
+++ RunSelector.C
@@ -3,10 +3,15 @@
 #include <TSelector.h>
 #include "TTree.h"
 
+// This small script only intends to make those three lines run in one shot
+
 void RunSelector(){
 
+  // Open your root file
   TFile *file = TFile::Open("MuonInfo_Run2017B_PRv1.root","READ");
+  // Pick up the tree
   TTree *tree = (TTree*)(file->Get("MuonPogTree/MUONPOGTREE"));
+  // This is how you run the Selector 
   tree->Process("MUONPOGTREE.C+");
 
 }
